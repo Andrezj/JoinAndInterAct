2404419e748bad25db946d6ed6f4b61727976b3f
Andrezj <andre.oliveira@ccc.ufcg.edu.br>
2020-11-27 13:18:04 -0300

    muudanças nos links na barra de navegaçao superior

--- Join&InterAct/frontend/app/src/components/top_main_navbar/index.js
+++ Join&InterAct/frontend/app/src/components/top_main_navbar/index.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
+import './styles.css';
 
 export default function MainNavbar() {
     return (
         <div style={{textAlign:"center"}}>
-            <h1>Join&InterAct</h1>
+            <h1 className="title">Join&InterAct</h1>
             <div className="topnav" style={{ padding:'20px'}}>
                 <a className="active" href="#home" style={{ padding:'10px'}}> Home</a>
                 <a href="#home" style={{ padding:'10px'}}>Play Safe</a>
-                <a href="#news" style={{ padding:'10px'}}>Your Profile</a>
+                <a href="#profile" style={{ padding:'10px'}}>Your Profile</a>
                 <a href="#contact" style={{ padding:'10px'}}>Friends</a>
                 <a href="#about" style={{ padding:'10px'}}>About</a>
-                <a href="#about" style={{ padding:'10px'}}>Sign Out</a>
+                <a href="#logout" style={{ padding:'10px'}}>Sign Out</a>
             </div>
         </div> 
     );
